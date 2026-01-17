cb4e985f4b3fa1e9a0a88bef87a0952cfbcf3a22
Toorum-SSG <alan13022011@gmail.com>
2026-01-17 12:28:29 +0100

    Classes added: DialogueOption, Inventory, Notebook, Quest

---
+++ src/Inventory.java
@@ -0,0 +1,18 @@
+public class Inventory {
+
+    public Inventory(int maxSlots) {}
+
+    public boolean addItem(Item item) {}
+
+    public boolean removeItem(String itemName) {}
+
+    public Item getItem(String itemName) {}
+
+    public boolean hasItem(String itemName) {}
+
+    public boolean hasSpace(int size) {}
+
+    public void showStatus() {}
+
+    public List<Item> getItems() {}
+}
---
+++ src/Notebook.java
@@ -0,0 +1,8 @@
+public class Notebook {
+
+    public Notebook() {}
+
+    public void addNote(String note) {}
+
+    public void showNotes() {}
+}
