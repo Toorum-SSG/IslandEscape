927d6d672410ebf11a90431fcd8d5bd0badb6fc4
Toorum-SSG <alan13022011@gmail.com>
2026-01-17 12:05:15 +0100

    added game class + it's method bodies

---
+++ src/Game.java
@@ -0,0 +1,14 @@
+public class Game {
+    public Game() {}
+    public void start(){}
+    private void showIntro(){}
+    private void initializeGame(){}
+    private void processCommand(String input){}
+    private void moveToLocation(String locationName){}
+    private void pickUpItem(String itemName) {}
+    private void dropItem(String itemName){}
+    private void talkToCharacter(String characterName){}
+    private void checkMarinakQuest(){}
+    private void checkKharQuest(){}
+    private void showHelp(){}
+}
